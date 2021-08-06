71b9e017bda028b1c6290d432f5a4ed2ddbc9411
angie <angiesan.0607@gmail.com>
2021-08-06 11:52:25 -0500

    fifth commit

---
+++ logic.js
@@ -0,0 +1,57 @@
+const progress = document.getElementById('progress')   /* me busca en el html para llamarlo a j.s */
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')  /* los llamo por clase con query.selector y con all para traerlos todos y me devuelve un array */
+
+let counterSteps = 1
+
+next.addEventListener('click', () => {
+    counterSteps++                               /* me le suma una osea contador =1 + 1*/
+    if (counterSteps > circles.lenght) {         /*lenght la longitud de mi array*/
+        counterSteps = circles.lenght
+    }
+    update()
+})
+
+prev.addEventListener('click', () => {
+    counterSteps--                              /* me resta una osea contador =1 + 1*/
+    if (counterSteps < 1) {         /*lenght la longitud de mi array*/
+        counterSteps = 1
+    }
+    update()
+})
+
+function update() {
+
+    circles.forEach((circle, id) => {
+        if(id < counterSteps){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')  
+
+
+    progress.style.width = (actives.length -1) / (circles.length-1) * 100 + '%'
+
+
+    if (counterSteps === 1) {
+
+        prev.disabled = true
+
+    } else if (counterSteps === circles.length) {
+        next.disabled = false
+
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+
+}
+
+
+
+
+
